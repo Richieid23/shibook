da81a1ce6c76471ebf67da9e6e55697451ef712e
Richieid23 <fitrarizki23@gmail.com>
2021-08-16 05:40:09 +0700

    add user fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username',
+        'roles',
+        'address',
+        'phone',
+        'avatar'
     ];
 
     /**

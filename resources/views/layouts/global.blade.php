9b67445b80dcc7df1a86e76c08ad751fd5f4f681
Richieid23 <fitrarizki23@gmail.com>
2021-08-19 19:29:43 +0700

    update menu

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -84,6 +84,12 @@
                             Manage Categories
                         </a>
                     </li>
+                     <li>
+                        <a href="{{ route('books.index') }}">
+                            <span class="oi oi-book"></span>
+                            Manage Books
+                        </a>
+                    </li>
                     <div class="d-block d-md-none">
                         <div class="dropdown-divider"></div>
                         <li>
@@ -138,5 +144,7 @@
         integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
         crossorigin="anonymous">
     </script>
+
+    @yield('footer-scripts')
 </body>
 </html>

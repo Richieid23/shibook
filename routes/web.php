4dfbfc049298c1293c3e27bda5b6730f92b87875
Richieid23 <fitrarizki23@gmail.com>
2021-08-19 19:31:31 +0700

    add book routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
+use App\Models\Book;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -28,9 +30,16 @@ Route::match(['GET', 'POST'], '/register', function () {
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::get('/ajax/categories/search', [CategoryController::class, 'ajaxSearch'])->name('ajax.search');
+
 Route::get('/categories/trash', [CategoryController::class, 'trash'])->name('categories.trash');
 Route::get('categories/{id}/restore', [CategoryController::class, 'restore'])->name('categories.restore');
 Route::delete('/categories/{category}/delete-permanent', [CategoryController::class, 'deletePermanent'])->name('categories.delete-permanent');
 
+Route::get('/books/trash', [BookController::class, 'trash'])->name('books.trash');
+Route::post('books/{book}/restore', [BookController::class, 'restore'])->name('books.restore');
+Route::delete('/books/{id}/delete-permanent', [BookController::class, 'deletePermanent'])->name('books.delete-permanent');
+
 Route::resource('users', UserController::class);
 Route::resource('categories', CategoryController::class);
+Route::resource('books', BookController::class);
